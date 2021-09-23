30d1c40bfcd7a0529a2d7f6d52da8448ed846cae
aaronli-uga <ql61608@uga.edu>
2021-09-22 21:19:33 -0400

    seems not working

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 const NPP = require('./newpieceplease')
 
-NPP.onready = () => {
-    console.log(NPP.orbitdb.id)
+NPP.onready = async () => {
+    console.log(NPP.pieces.id)
+    const cid = await NPP.addNewPiece("QmNR2n4zywCV61MeMLB6JwPueAPqheqpfiA4fLPMxouEmQ")
+    console.log("cid:", cid)
+    const content = NPP.node.dag.get(cid)
+    console.log(content.value.payload)
  }
  
 NPP.create()
--- newpieceplease.js
+++ newpieceplease.js
@@ -17,17 +17,33 @@ class NewPiecePlease {
   
       this._init()
     }
+
+    async addNewPiece(hash, instrument = 'Piano') {
+        // const existingPiece = this.getPieceByHash(hash)
+        // if(existingPiece){
+        //     await this.updatePieceByHash(hash, instrument)
+        //     return
+        // }
+
+        const cid = await this.pieces.put({hash, instrument})
+        return cid
+    }
   
     async _init () {
-      this.orbitdb = await this.OrbitDB.createInstance(this.node)
-      this.defaultOptions = { accessController: {
-        write: [this.orbitdb.identity.id]
+        this.orbitdb = await this.OrbitDB.createInstance(this.node)
+        this.defaultOptions = { accessController: { write: [this.orbitdb.identity.id]}}
+
+        const docStoreOptions = {
+            ...this.defaultOptions,
+            indexBy: 'hash',
         }
-      }
-      if(this.onready) this.onready();
+
+        this.pieces = await this.orbitdb.docstore('pieces', docStoreOptions)
+        await this.pieces.load()
+
+        await this.onready();
     }
 }
-
 
 
 try {
@@ -37,3 +53,15 @@ try {
 } catch (e) {
     window.NPP = new NewPiecePlease(window.Ipfs, window.OrbitDB)
 }
+
+
+const NPP = require('./newpieceplease')
+
+NPP.onready = async () => {
+    console.log(NPP.pieces.id)
+    const cid = await NPP.addNewPiece("QmNR2n4zywCV61MeMLB6JwPueAPqheqpfiA4fLPMxouEmQ")
+    const content = await NPP.node.dag.get(cid)
+    console.log(content.value.payload)
+}
+
+NPP.create()
